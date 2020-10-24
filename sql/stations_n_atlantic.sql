ea69f5737e5d3cd704b7769f179cfbbb04505edc
rossrco <rossrco@gmail.com>
2020-10-24 16:07:01 +0200

    add country name to the stations in the north atlantic query

--- sql/stations_n_atlantic.sql
+++ sql/stations_n_atlantic.sql
@@ -1,23 +1,30 @@
-# find all weather stations in a custom polygon in the North Atlantic Ocean
+WITH stations AS (
+  SELECT
+    id,
+    LEFT(id, 2) as country,
+    state,
+    name,
+    latitude,
+    longitude
+  FROM `bigquery-public-data.ghcn_d.ghcnd_stations`
+)
 
-select
-id,
-state,
-name,
-latitude,
-longitude
-from `bigquery-public-data.ghcn_d.ghcnd_stations`
+SELECT
+stations.*,
+countries.name
+FROM stations
+LEFT JOIN `bigquery-public-data.ghcn_d.ghcnd_countries` countries ON stations.country = countries.code
 WHERE
 ST_CONTAINS(
   ST_MAKEPOLYGON(
     ST_MAKELINE(
       ARRAY(
-              select ST_GEOGPOINT(-12.950869, 53.431059) union all
-              select ST_GEOGPOINT(-10.342842, 35.002107) union all
-              select ST_GEOGPOINT(-19.931662, 13.986127) union all
-              select ST_GEOGPOINT(-9.352620, 0.0)        union all
-              select ST_GEOGPOINT(-49.101915, 0.384726)  union all
-              select ST_GEOGPOINT(-71.903541, 13.516013)
+              SELECT ST_GEOGPOINT(-12.950869, 53.431059) UNION ALL
+              SELECT ST_GEOGPOINT(-10.342842, 35.002107) UNION ALL
+              SELECT ST_GEOGPOINT(-19.931662, 13.986127) UNION ALL
+              SELECT ST_GEOGPOINT(-9.352620, 0.0)        UNION ALL
+              SELECT ST_GEOGPOINT(-49.101915, 0.384726)  UNION ALL
+              SELECT ST_GEOGPOINT(-71.903541, 13.516013)
             )
           )
         )
