8d8b009b852b081e92fe957239a18285f47b9d80
rossrco <rossrco@gmail.com>
2020-11-08 12:43:57 +0100

    add a query that joins hurricanes to weather station data

---
+++ sql/stations_hrcn_data.sql
@@ -0,0 +1,29 @@
+WITH
+  hrcn AS (
+  SELECT
+    sid,
+    DATE(iso_time) AS iso_date,
+    latitude,
+    longitude,
+    WMO_WIND
+  FROM
+    `bigquery-public-data.noaa_hurricanes.hurricanes`)
+SELECT
+  sid,
+  iso_date,
+  date,
+  latitude,
+  longitude,
+  WMO_WIND,
+  value AS avg_temp
+FROM
+  hrcn
+LEFT JOIN
+  `bigquery-public-data.ghcn_d.ghcnd_1973` ghcnd_1973
+ON
+  hrcn.iso_date = date(ghcnd_1973.date)
+  AND ghcnd_1973.id = 'GPM00078894'
+  AND ghcnd_1973.element ='TAVG'
+WHERE EXTRACT(year FROM iso_date) = 1973
+AND date IS NOT NULL
+AND WMO_WIND IS NOT NULL
